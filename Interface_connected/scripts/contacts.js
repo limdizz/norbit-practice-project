be018d5e6ae4ccaa8721afd51f144a1cf14e9230
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2026-03-10 22:48:25 +0300

    replace feedback form with FAQ section

--- Interface_connected/scripts/contacts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("feedback-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Спасибо за сообщение! Мы свяжемся с вами в ближайшее время.");
-    this.reset();
-});
